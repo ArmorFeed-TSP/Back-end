0d901d18305ce136bd0348b018162c5428aef72d
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 11:25:48 -0500

    feat: Added Vehicles Resource Layer

---
+++ ArmorFeedApi/ArmorFeedApi/Vehicles/Resources/SaveVehicleResource.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ArmorFeedApi.Vehicles.Resources;
+
+public class SaveVehicleResource
+{
+    [Required]
+    [MaxLength(50)]
+    public string Brand { get; set; }
+    
+    [MaxLength(120)]
+    public string LicensePlate { get; set; }
+    [MaxLength(3000)]
+    public int Year { get; set; }
+    [MaxLength(120)]
+    public string Model { get; set; }
+    [MaxLength(120)]
+    public string MaintenanceDate { get; set; }
+    [MaxLength(120)]
+    public string VehicleType { get; set; }
+    [Required]
+    public int EnterpriseId { get; set; }
+    
+}
