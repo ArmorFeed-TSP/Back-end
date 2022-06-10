b0ed04242c9767243c0a2c796ee8cc544ab46229
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 11:40:02 -0500

    feat: Added enterprise service layer

---
+++ ArmorFeedApi/ArmorFeedApi/Shipments/Services/EnterpriseService.cs
@@ -0,0 +1,82 @@
+﻿
+using ArmorFeedApi.Shared.Domain.Repositories;
+using ArmorFeedApi.Shipments.Domain.Models;
+using ArmorFeedApi.Shipments.Domain.Repositories;
+using ArmorFeedApi.Shipments.Domain.Services;
+using ArmorFeedApi.Shipments.Domain.Services.Communication;
+
+namespace ArmorFeedApi.Shipments.Services;
+
+public class EnterpriseService:IEnterpriseService
+{
+    private readonly IEnterpriseRepository _enterpriseRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public EnterpriseService(IEnterpriseRepository enterpriseRepository, IUnitOfWork unitOfWork)
+    {
+        _enterpriseRepository = enterpriseRepository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<IEnumerable<Enterprise>> ListAsync()
+    {
+        return await _enterpriseRepository.ListAsync();
+    }
+
+    public async Task<EnterpriseResponse> SaveAsync(Enterprise enterprise)
+    {
+        try
+        {
+            await _enterpriseRepository.AddAsync(enterprise);
+            await _unitOfWork.CompleteAsync();
+
+            return new EnterpriseResponse(enterprise);
+        }
+        catch (Exception e)
+        {
+            return new EnterpriseResponse($"An error occurred while saving the category: {e.Message}");
+        }
+    }
+
+    public async Task<EnterpriseResponse> UpdateAsync(int id, Enterprise enterprise)
+    {
+        var existingEnterprise = await _enterpriseRepository.FindByIdAsync(id);
+
+        if (existingEnterprise == null)
+            return new EnterpriseResponse("Category not found.");
+
+        existingEnterprise.Name = enterprise.Name;
+
+        try
+        {
+            _enterpriseRepository.Update(existingEnterprise);
+            await _unitOfWork.CompleteAsync();
+
+            return new EnterpriseResponse(existingEnterprise);
+        }
+        catch (Exception e)
+        {
+            return new EnterpriseResponse($"An error occurred while updating the category: {e.Message}");
+        }
+    }
+
+    public async Task<EnterpriseResponse> DeleteAsync(int id)
+    {
+        var existingEnterprise = await _enterpriseRepository.FindByIdAsync(id);
+
+        if (existingEnterprise == null)
+            return new EnterpriseResponse("Category not found.");
+
+        try
+        {
+            _enterpriseRepository.Remove(existingEnterprise);
+            await _unitOfWork.CompleteAsync();
+
+            return new EnterpriseResponse(existingEnterprise);
+        }
+        catch (Exception e)
+        {
+            return new EnterpriseResponse($"An error occurred while deleting the category: {e.Message}");
+        }
+    }
+}
