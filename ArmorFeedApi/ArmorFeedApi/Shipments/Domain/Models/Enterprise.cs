c6e16e974ac605cb163e127380952f4b6eb70916
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 11:36:51 -0500

    feat: Added enterprise model

--- ArmorFeedApi/ArmorFeedApi/Shipments/Domain/Models/Enterprise.cs
+++ ArmorFeedApi/ArmorFeedApi/Shipments/Domain/Models/Enterprise.cs
@@ -1,6 +1,19 @@
-﻿namespace ArmorFeedApi.Shipments.Domain.Models;
+﻿using ArmorFeedApi.Vehicles.Domain.Models;
+
+namespace ArmorFeedApi.Shipments.Domain.Models;
 
 public class Enterprise
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Ruc { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public float PriceBase { get; set; }
+    public float FactorWeight { get; set; }
+    public int ShippingTime { get; set; }
+    public float Score { get; set; }
+    public string Photo { get; set; }
     
+    public IList<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
 }
