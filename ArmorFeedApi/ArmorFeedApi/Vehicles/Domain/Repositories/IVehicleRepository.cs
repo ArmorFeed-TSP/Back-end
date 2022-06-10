c2540d297adc11447fea0ce41e82d1a0501e5134
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 11:24:39 -0500

    feat: Added Vehicles Domain Layer

---
+++ ArmorFeedApi/ArmorFeedApi/Vehicles/Domain/Repositories/IVehicleRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using ArmorFeedApi.Vehicles.Domain.Models;
+
+namespace ArmorFeedApi.Vehicles.Domain.Repositories;
+
+public interface IVehicleRepository
+{
+    Task<IEnumerable<Vehicle>> ListAsync();
+    Task AddAsync(Vehicle vehicle);
+    Task<Vehicle> FindByIdAsync(int vehicleId);
+    Task<Vehicle> FindByBrandAsync(string brand);
+    Task<IEnumerable<Vehicle>> FindByEnterpriseIdAsync(int enterpriseId);
+    void Update(Vehicle vehicle);
+    void Remove(Vehicle vehicle);
+}
---
+++ ArmorFeedApi/ArmorFeedApi/Vehicles/Domain/Services/IVehicleService.cs
@@ -0,0 +1,14 @@
+﻿
+using ArmorFeedApi.Vehicles.Domain.Models;
+using ArmorFeedApi.Vehicles.Domain.Services.Communication;
+
+namespace ArmorFeedApi.Vehicles.Domain.Services;
+
+public interface IVehicleService
+{
+    Task<IEnumerable<Vehicle>> ListAsync();
+    Task<IEnumerable<Vehicle>> ListByEnterpriseIdAsync(int enterpriseId);
+    Task<VehicleResponse> SaveAsync(Vehicle vehicle);
+    Task<VehicleResponse> UpdateAsync(int vehicleId, Vehicle vehicle);
+    Task<VehicleResponse> DeleteAsync(int vehicleId);
+}
