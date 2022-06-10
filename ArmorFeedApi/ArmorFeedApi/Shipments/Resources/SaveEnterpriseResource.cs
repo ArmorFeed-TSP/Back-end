997ab24d5f8bdd7bc90843e2aabff8f6d04901ae
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 11:39:45 -0500

    feat: Added enterprise resource layer

---
+++ ArmorFeedApi/ArmorFeedApi/Shipments/Resources/SaveEnterpriseResource.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningCenter.API.Learning.Resources;
+
+public class SaveEnterpriseResource
+{
+    [Required]
+    public string Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+}
