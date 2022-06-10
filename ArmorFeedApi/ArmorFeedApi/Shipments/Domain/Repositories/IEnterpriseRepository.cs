d712b3bcd47e0023357727ca75bf8e3e9a8710d3
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 11:37:10 -0500

    feat: Added enterprise repositories

---
+++ ArmorFeedApi/ArmorFeedApi/Shipments/Domain/Repositories/IEnterpriseRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using ArmorFeedApi.Shipments.Domain.Models;
+
+namespace ArmorFeedApi.Shipments.Domain.Repositories;
+
+public interface IEnterpriseRepository
+{
+    Task<IEnumerable<Enterprise>> ListAsync();
+    Task AddAsync(Enterprise enterprise);
+    Task<Enterprise> FindByIdAsync(int id);
+    void Update(Enterprise enterprise);
+    void Remove(Enterprise enterprise);
+}
