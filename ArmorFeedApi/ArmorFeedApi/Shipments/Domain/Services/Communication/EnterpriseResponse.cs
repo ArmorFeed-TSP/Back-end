fafd1dfdf6c087cb49787a3812fbda63e3d7eb07
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 11:38:58 -0500

    feat: Added enterprise domain layer

---
+++ ArmorFeedApi/ArmorFeedApi/Shipments/Domain/Services/Communication/EnterpriseResponse.cs
@@ -0,0 +1,16 @@
+﻿
+using ArmorFeedApi.Shared.Domain.Services.Communication;
+using ArmorFeedApi.Shipments.Domain.Models;
+
+namespace ArmorFeedApi.Shipments.Domain.Services.Communication;
+
+public class EnterpriseResponse: BaseResponse<Enterprise>
+{
+    public EnterpriseResponse(Enterprise resource) : base(resource)
+    {
+    }
+
+    public EnterpriseResponse(string message) : base(message)
+    {
+    }
+}
---
+++ ArmorFeedApi/ArmorFeedApi/Shipments/Domain/Services/IEnterpriseService.cs
@@ -0,0 +1,14 @@
+﻿
+
+using ArmorFeedApi.Shipments.Domain.Models;
+using ArmorFeedApi.Shipments.Domain.Services.Communication;
+
+namespace ArmorFeedApi.Shipments.Domain.Services;
+
+public interface IEnterpriseService
+{
+    Task<IEnumerable<Enterprise>> ListAsync();
+    Task<EnterpriseResponse> SaveAsync(Enterprise enterprise);
+    Task<EnterpriseResponse> UpdateAsync(int id, Enterprise enterprise);
+    Task<EnterpriseResponse> DeleteAsync(int id);
+}
