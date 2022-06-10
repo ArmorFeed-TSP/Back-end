82509f8237fb7a8eab29cb640cd369563bcf7bed
Nilton Torres <u201924058@upc.edu.pe>
2022-06-10 12:33:22 -0500

    feat: Added tables for vehicles and enterprises

--- ArmorFeedApi/ArmorFeedApi/Shared/Persistence/Contexts/AppDbContext.cs
+++ ArmorFeedApi/ArmorFeedApi/Shared/Persistence/Contexts/AppDbContext.cs
@@ -50,6 +50,37 @@ public class AppDbContext: DbContext
         // With Shipment
         builder.Entity<ShipmentReview>().HasOne(s => s.Shipment);
         
+        //Vehicles
+        builder.Entity<Vehicle>().ToTable("Vehicles");
+        builder.Entity<Vehicle>().HasKey(p => p.Id);
+        builder.Entity<Vehicle>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Vehicle>().Property(p => p.Brand).IsRequired().HasMaxLength(120);
+        builder.Entity<Vehicle>().Property(p => p.Year).IsRequired().HasMaxLength(120);
+        builder.Entity<Vehicle>().Property(p => p.Model).IsRequired();
+        builder.Entity<Vehicle>().Property(p => p.LicensePlate).IsRequired();
+        builder.Entity<Vehicle>().Property(p => p.VehicleType).IsRequired();
+        
+        //Relationships
+        builder.Entity<Enterprise>()
+            .HasMany(p => p.Vehicles)
+            .WithOne(p => p.Enterprise)
+            .HasForeignKey(p => p.EnterpriseId);
+        
+        //Enterprise
+        builder.Entity<Enterprise>().ToTable("Enterprises");
+        builder.Entity<Enterprise>().HasKey(s => s.Id);
+        builder.Entity<Enterprise>().Property(s => s.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Enterprise>().Property(s => s.Name).IsRequired().HasMaxLength(50);
+        builder.Entity<Enterprise>().Property(s => s.Ruc).IsRequired().HasMaxLength(50);
+        builder.Entity<Enterprise>().Property(s => s.PhoneNumber).IsRequired();
+        builder.Entity<Enterprise>().Property(s => s.Email).IsRequired();
+        builder.Entity<Enterprise>().Property(s => s.PriceBase).IsRequired();
+        builder.Entity<Enterprise>().Property(s => s.FactorWeight).IsRequired();
+        builder.Entity<Enterprise>().Property(s => s.ShippingTime).IsRequired();
+        builder.Entity<Enterprise>().Property(s => s.Score).IsRequired();
+        builder.Entity<Enterprise>().Property(s => s.Photo).IsRequired();
+
+
         //Apply Snake Case Naming Conventios
         builder.UseSnakeCaseNamingConvention();
     }
