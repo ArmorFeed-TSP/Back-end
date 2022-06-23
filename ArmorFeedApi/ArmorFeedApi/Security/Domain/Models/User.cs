53f92e606452894841d8dbf1d9d8effd54c8312c
price <nicole_14_2015@hotmail.com>
2022-06-22 21:10:01 -0500

    feat: Added User Domain Entity.

---
+++ ArmorFeedApi/ArmorFeedApi/Security/Domain/Models/User.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace ArmorFeedApi.Security.Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public byte[] Photo { get; set; }
+    public string Ruc { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    [JsonIgnore]
+    public string Password { get; set; }
+}
