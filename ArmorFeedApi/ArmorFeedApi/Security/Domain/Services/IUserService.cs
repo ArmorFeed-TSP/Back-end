49f453bd1faac71925e5f4104bbc835676a36940
price <nicole_14_2015@hotmail.com>
2022-06-22 21:27:50 -0500

    feat: Added Domain Service Layer Contract.

---
+++ ArmorFeedApi/ArmorFeedApi/Security/Domain/Services/IUserService.cs
@@ -0,0 +1,14 @@
+using ArmorFeedApi.Security.Domain.Models;
+using ArmorFeedApi.Security.Domain.Services.Communication;
+
+namespace ArmorFeedApi.Security.Domain.Services;
+
+public interface IUserService
+{
+    Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetByIdAsync(int id);
+    Task RegisterAsync(RegisterRequest request);
+    Task UpdateAsync(int id, UpdateRequest request);
+    Task DeleteAsync(int id);
+}
