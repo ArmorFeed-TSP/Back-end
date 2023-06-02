306e1e3277245d0df5ca802895f122bff0e395b9
Jesús Pacheco <jesuspacheco0219@gmail.com>
2023-06-02 00:50:49 -0300

    add refactor controller

--- ArmorFeedApi/ArmorFeedApi/Comments/Controllers/CommentsController.cs
+++ ArmorFeedApi/ArmorFeedApi/Comments/Controllers/CommentsController.cs
@@ -7,57 +7,58 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
-namespace ArmorFeedApi.Comments.Controllers;
-[ApiController]
-[Route("/api/v1/[controller]")]
-[Produces(MediaTypeNames.Application.Json)]
-[SwaggerTag("Create, Read, Update and Delete Comments")]
-public class CommentsController:ControllerBase
+namespace ArmorFeedApi.Comments.Controllers
 {
-    private readonly ICommentService _commentService;
-    private readonly IMapper _mapper;
-    public CommentsController(ICommentService commentService, IMapper mapper)
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [SwaggerTag("Create, Read, Update and Delete Comments")]
+    public class CommentsController : ControllerBase
     {
-        _commentService = commentService;
-        _mapper = mapper;
+        private readonly ICommentService _commentService;
+        private readonly IMapper _mapper;
+
+        public CommentsController(ICommentService commentService, IMapper mapper)
+        {
+            _commentService = commentService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(
+            Summary = "Get All Comments",
+            Description = "Get All Comment",
+            OperationId = "GetComments",
+            Tags = new[] { "Comments" }
+        )]
+        public async Task<IEnumerable<CommentResource>> GetAllAsync()
+        {
+            var comments = await _commentService.ListAsync();
+            var resources = _mapper.Map<IEnumerable<Comment>, IEnumerable<CommentResource>>(comments);
+
+            return resources;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(
+            Summary = "Post Comment",
+            Description = "Save Comment In Database",
+            OperationId = "PostComment",
+            Tags = new[] { "Comments" }
+        )]
+        public async Task<IActionResult> PostAsync([FromBody] SaveCommentResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+
+            var comment = _mapper.Map<SaveCommentResource, Comment>(resource);
+            var result = await _commentService.SaveAsync(comment);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            var commentResource = _mapper.Map<Comment, CommentResource>(result.Resource);
+            return Ok(commentResource);
+        }
     }
-    [HttpGet]
-    [SwaggerOperation(
-        Summary = "Get All Comments",
-        Description = "Get All Comment",
-        OperationId = "GetComments",
-        Tags = new []{"Comments"}
-    )]
-    public async Task<IEnumerable<CommentResource>> GetAllAsync()
-    {
-        var comments = await _commentService.ListAsync();
-        var resources = _mapper.Map<IEnumerable<Comment>, IEnumerable<CommentResource>>(comments);
-
-        return resources;
-    }
-    [HttpPost]
-    [SwaggerOperation(
-        Summary = "Post Comment",
-        Description = "Save Comment In Database",
-        OperationId = "PostComment",
-        Tags = new []{"Comments"}
-    )]
-    
-    public async Task<IActionResult> PostAsync([FromBody] SaveCommentResource resource)
-    {
-        if (!ModelState.IsValid)
-            return BadRequest(ModelState.GetErrorMessages());
-
-        var comment = _mapper.Map<SaveCommentResource, Comment>(resource);
-
-        var result = await _commentService.SaveAsync(comment);
-
-        if (!result.Success)
-            return BadRequest(result.Message);
-
-        var commentResource = _mapper.Map<Comment, CommentResource>(result.Resource);
-
-        return Ok(commentResource);
-    }
-    
 }
--- ArmorFeedApi/ArmorFeedApi/Comments/Controllers/ShipmentCommentController.cs
+++ ArmorFeedApi/ArmorFeedApi/Comments/Controllers/ShipmentCommentController.cs
@@ -5,31 +5,33 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
-namespace ArmorFeedApi.Comments.Controllers;
-
-[ApiController]
-[Route("/api/v1/shipment/{shipmentId}/comments")] 
-public class ShipmentCommentController:ControllerBase
+namespace ArmorFeedApi.Comments.Controllers
 {
-    private readonly ICommentService _commentService;
-    private readonly IMapper _mapper;
+    [ApiController]
+    [Route("/api/v1/shipment/{shipmentId}/comments")]
+    public class ShipmentCommentController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+        private readonly IMapper _mapper;
 
-    public ShipmentCommentController(ICommentService commentService, IMapper mapper)
-    {
-        _commentService = commentService;
-        _mapper = mapper;
-    }
-    [HttpGet]
-    [SwaggerOperation(
-        Summary = "Get Comments",
-        Description = "Get All Comments by Shipment Id",
-        OperationId = "GetComments",
-        Tags = new []{"Comments"}
-    )]
-    public async Task<IEnumerable<CommentResource>> GetAllByShipmentId(int shipmentId)
-    {
-        var comments = await _commentService.ListByShipmentAsync(shipmentId);
-        var resources = _mapper.Map<IEnumerable<Comment>, IEnumerable<CommentResource>>(comments);
-        return resources;
+        public ShipmentCommentController(ICommentService commentService, IMapper mapper)
+        {
+            _commentService = commentService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(
+            Summary = "Get Comments",
+            Description = "Get All Comments by Shipment Id",
+            OperationId = "GetComments",
+            Tags = new[] { "Comments" }
+        )]
+        public async Task<IEnumerable<CommentResource>> GetAllByShipmentId(int shipmentId)
+        {
+            var comments = await _commentService.ListByShipmentAsync(shipmentId);
+            var resources = _mapper.Map<IEnumerable<Comment>, IEnumerable<CommentResource>>(comments);
+            return resources;
+        }
     }
 }
